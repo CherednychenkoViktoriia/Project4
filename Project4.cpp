18e4f12bac0f7c6aa0331e67b8fc686cae141f25
Viktoriia <viktoryacherednichenko@gmail.com>
2020-12-14 15:35:15 +0200

    Opening 1 app in 2 app with CreateProcess

--- Project4.cpp
+++ Project4.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <windows.h>
 
-int main()
+int main(int argc, char** argv)
 {   
-
+	STARTUPINFO si = {};
+	PROCESS_INFORMATION pi = {};
+	wchar_t args[] = L"somepath C:\\tmp\\Project3\\file.txt";
+	Sleep(5000);
+	if (!CreateProcess(L"C:\\tmp\\Project3\\Debug\\Project3.exe",
+		args,
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi)
+		)
+	{
+	printf("CreateProcess failed (%d).\n",
+		GetLastError());
+		return 0;
+	}	
+	return 0;
 }
